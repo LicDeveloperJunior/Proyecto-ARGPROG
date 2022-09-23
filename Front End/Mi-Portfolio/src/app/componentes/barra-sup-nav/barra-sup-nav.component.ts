4ee2d32c3077569dafce43862e3acefaee2ef65e
LicDeveloperJunior <agustincv1997@gmail.com>
2022-09-23 14:16:40 -0300

    Realizado responsive de componente barra-lat-sup

--- Front End/Mi-Portfolio/src/app/componentes/barra-sup-nav/barra-sup-nav.component.ts
+++ Front End/Mi-Portfolio/src/app/componentes/barra-sup-nav/barra-sup-nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faInstagram, faFacebook, faWhatsapp, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faMoon, faSun, faBars } from '@fortawesome/free-solid-svg-icons';
 import { TemaService } from 'src/app/servicios/multitemas/tema.service';
 
 @Component({
@@ -8,21 +9,33 @@ import { TemaService } from 'src/app/servicios/multitemas/tema.service';
   styleUrls: ['./barra-sup-nav.component.less']
 })
 export class BarraSupNavComponent implements OnInit {
-  datos:any;
+  datos: any;
   insta = faInstagram;
   face = faFacebook;
   what = faWhatsapp;
   git = faGithub;
   link = faLinkedin;
+  dark = faMoon;
+  light = faSun;
+  menu = faBars;
+
+  temaActual: string;
+
   constructor(private temaServicio: TemaService) {
-    this.cambiarTema(this.temaServicio.getTema())
-   }
+    this.temaActual = this.temaServicio.getTema();
+    this.temaServicio.setTema(this.temaServicio.getTema());
+  }
 
   ngOnInit(): void {
   }
 
-  cambiarTema(nombre: string) {
-    this.temaServicio.setTema(nombre);
+  cambiarTema() {
+    if (this.temaActual === "default") {
+      this.temaActual = "dark";
+      this.temaServicio.setTema("dark");
+    } else {
+      this.temaServicio.setTema("default");
+      this.temaActual = "default";
+    }
   }
-
 }
